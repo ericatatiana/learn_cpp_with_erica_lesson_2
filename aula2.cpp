175940a883684d90a58f122f31adf0492e62aa4f
Erica Tatiana <143865619+ericatatiana@users.noreply.github.com>
2026-04-19 12:39:35 +0200

    Add basic C++ program to display user info

---
+++ aula2.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std;
+int main() {
+  string nome = "Pessoa"; 
+  int idade = 21; 
+  float altura = 1.65;
+  cout << "Nome: " << nome << endl; cout << "Idade: " << idade << endl; 
+  cout << "Altura: " << altura << endl; return 0;
+}
